ed0e1d043489d3aee6dffacfc7866a95cc01b7d1
djmonnar <djmonnar@users.noreply.github.com>
2026-06-09 21:46:21 +0900

    Temporarily disable video popup

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 import { Hero } from "@/components/Hero";
 import { MapSection } from "@/components/MapSection";
 import { Profile } from "@/components/Profile";
-import { VideoPopup } from "@/components/VideoPopup";
 
 export default function Home() {
   return (
@@ -9,7 +8,6 @@ export default function Home() {
       <Hero />
       <Profile />
       <MapSection />
-      <VideoPopup />
     </main>
   );
 }
